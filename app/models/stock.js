9ed9f7c2dfd7b418b0931aedb951f3d0bcd2a81f
Dave Boling <daviddboling@gmail.com>
2014-07-21 10:56:41 -0500

    Created stock constructor

--- app/models/stock.js
+++ app/models/stock.js
@@ -1,5 +1,11 @@
+'use strict';
 
 
+function Stock(symbol, count){
+  this.symbol = symbol.toUpperCase();
+  this.count  = parseInt(count);
+  this.price = 0;
+}
 
 
 
--- test/unit/stock.js
+++ test/unit/stock.js
@@ -1,3 +1,18 @@
+
+'use strict';
+/*global describe, it*/
 var expect = require('chai').expect;
 var Stock = require('../../app/models/stock');
+
+describe('Stock', function(){
+  describe('constructor', function(){
+    it('should create a new stock object', function(){
+      var aapl = new Stock('aApL', '100');
+
+      expect(aapl).to.be.instanceof(Stock);
+      expect(aapl.symbol).to.equal('AAPL');
+      expect(aapl.count).to.equal(100);
+    });
+  });
+});
 
