7890a5061c5050be1686bec6787d3ef2ddad6934
David Boling <daviddboling@gmail.com>
2014-07-21 15:14:05 -0500

    Added ability to add stocks to portfolios

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -1,10 +1,39 @@
 'use strict';
 
+var Stock = require('./stock');
+
 function Portfolio(name){
   this.name  = name;
   this.stock = [];
 
 }
+
+Portfolio.prototype.add = function(symbol, amount){
+  //This variable is determined by findStock()
+  var stock = findStock(this.stock, symbol); 
+
+  //Check to see if is an object, returns true if object 
+  //and false if the variable is undefined
+  if(stock){
+    stock.count += amount;
+  //If stock ISN'T already an object, it will initialize stock as a new
+  //object and push it to the portfolio array
+  }else{
+    stock = new Stock(symbol, amount);
+    this.stock.push(stock);
+  }
+};
+
+//Loops through stock to see if a stock symbol already
+//exists. If it does, the function will return the entire object
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].symbol === symbol.toUpperCase()){
+      return stocks[i];
+    }
+  }
+}
+
 
 
 
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -8,11 +8,24 @@ describe('Portfolio', function(){
   describe('constructor', function(){
     it('should create a new portfolio object', function(){
       var p1 = new Portfolio('name');
-
       expect(p1).to.be.instanceof(Portfolio);
       expect(p1.name).to.equal('name');
       expect(p1.stock.length).to.equal(0);
+    });
+  });
+  describe('#add', function(){
+    it('should add stocks to a given portfolio', function(){
+      var p1 = new Portfolio('Tech');
 
+      p1.add('aapl', 50);
+      p1.add('aapl', 25);
+      p1.add('amzn', 50);
+
+      expect(p1.stock[0].symbol).to.equal('AAPL');
+      expect(p1.stock[0].count).to.equal(75);
+      expect(p1.stock[1].symbol).to.equal('AMZN');
+      expect(p1.stock[1].count).to.equal(50);
+      
     });
   });
 });
