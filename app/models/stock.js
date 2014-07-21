e84595139cfd15375533ba5ea9f978bfd167295f
jessica raines <jessicafraines@gmail.com>
2014-07-21 13:22:11 -0500

    added class method getQuote

--- app/models/stock.js
+++ app/models/stock.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var request = require('request');
 
 function Stock(symbol, count){
   this.symbol = symbol.toUpperCase();
@@ -7,7 +8,14 @@ function Stock(symbol, count){
   this.price = 0;
 }
 
+Stock.getQuote = function(symbol, cb){
+  var url = 'http://dev.markitondemand.com/Api/v2/Quote/json?symbol=' + symbol.toUpperCase();
 
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    cb(body.LastPrice);
+  });
+};
 
 
 
--- test/unit/stock.js
+++ test/unit/stock.js
@@ -12,6 +12,15 @@ describe('Stock', function(){
       expect(aapl).to.be.instanceof(Stock);
       expect(aapl.symbol).to.equal('AAPL');
       expect(aapl.count).to.equal(100);
+      expect(aapl.price).to.equal(0);
+    });
+  });
+  describe('.getQuote', function() {
+    it('should get a quote from a web service', function(done){
+      Stock.getQuote('aapl', function(quote) {
+        expect(quote).to.be.at.least(0);
+        done(); //done is a function from the Chai library
+      });
     });
   });
 });
